2eefc158d991850928e2189db0e8af18c4e1b3bb
Alankrutha Reddy <85304096+Alankrutha181@users.noreply.github.com>
2024-07-30 17:31:16 -0400

    Add files via upload

---
+++ Where_clause_practice.sql
@@ -0,0 +1,7 @@
+SELECT *
+FROM parks_and_recreation.employee_demographics
+WHERE gender = 'Female';
+
+select *
+FROM parks_and_recreation.employee_demographics
+WHERE first_name = 'Chris' AND age =43;
